5fbe041e8dce564cd5de91963e3447289fa00151
Dany <dxavi1212@gmail.com>
2016-07-31 16:22:03 -0500

    archivos

---
+++ app/src/main/java/com/facci/restaurantdl/MainActivityDL.java
@@ -0,0 +1,42 @@
+package com.facci.restaurantdl;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivityDL extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_activity_dl);
+    }
+    public void Click1(View v){
+        Toast.makeText(MainActivityDL.this, "Brocheta de pollo", Toast.LENGTH_SHORT).show();
+    }
+
+    public void Click2(View v){
+        Toast.makeText(MainActivityDL.this, "Pollo a la naranja", Toast.LENGTH_SHORT).show();
+    }
+
+    public void Click3(View v){
+        Toast.makeText(MainActivityDL.this, "Chuleta asada", Toast.LENGTH_SHORT).show();
+    }
+
+    public void Click4(View v){
+        Toast.makeText(MainActivityDL.this, "Hamburguesa", Toast.LENGTH_SHORT).show();
+    }
+
+    public void Click5(View v){
+        Toast.makeText(MainActivityDL.this, "Arroz colorado", Toast.LENGTH_SHORT).show();
+    }
+
+    public void Click6(View v){
+        Toast.makeText(MainActivityDL.this, "Pizza", Toast.LENGTH_SHORT).show();
+    }
+
+    public void Click7(View v){
+        Toast.makeText(MainActivityDL.this, "Tacos", Toast.LENGTH_SHORT).show();
+    }
+}
